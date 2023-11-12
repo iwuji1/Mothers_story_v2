ecf95033babc995bdc3615d66d3ddede440680a6
iwuji1 <obinnaops@gmail.com>
2023-11-12 21:06:05 +0000

    include functionality seperately 2.4

--- mothers/.netlify/server/manifest.js
+++ mothers/.netlify/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["favicon.png"]),
 	mimeTypes: {".png":"image/png"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.f062ad17.js","app":"_app/immutable/entry/app.97c419cd.js","imports":["_app/immutable/entry/start.f062ad17.js","_app/immutable/chunks/scheduler.1f6706a1.js","_app/immutable/chunks/singletons.9953c3df.js","_app/immutable/entry/app.97c419cd.js","_app/immutable/chunks/scheduler.1f6706a1.js","_app/immutable/chunks/index.a70b2643.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.3f24c5a0.js","app":"_app/immutable/entry/app.e7465fb6.js","imports":["_app/immutable/entry/start.3f24c5a0.js","_app/immutable/chunks/scheduler.1f6706a1.js","_app/immutable/chunks/singletons.fac38b8d.js","_app/immutable/entry/app.e7465fb6.js","_app/immutable/chunks/scheduler.1f6706a1.js","_app/immutable/chunks/index.a70b2643.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
